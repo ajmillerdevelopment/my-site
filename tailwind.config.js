20c7d19775e740f4e5a251c9f2139a38b5fc0996
Alex Miller <ajm9594@gmail.com>
2021-05-11 15:23:21 -0700

    custom colors + boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        highlightBlue: '#6575E0',
+        backgroundGrey: '#E5E7DC',
+      }
+    },
   },
   variants: {
     extend: {},
