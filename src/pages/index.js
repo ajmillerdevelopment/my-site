9726150bc341588efc48599527608b454a70b40b
Alex Miller <ajm9594@gmail.com>
2021-05-11 16:48:51 -0700

    index page started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import * as React from "react"
-
+import Headshot from '../images/cropped.jpg'
 const IndexPage = () => {
   return (
     <div className="w-screen flex flex-col items-center h-screen bg-backgroundGrey">
-      <h1>ALEXANDER MILLER</h1>
+      <h1 className="mt-4 mb-1 font-display text-center text-shadow text-highlightBlue"> <span className="text-4xl">ALEXANDER</span> <br /> <span className="text-7xl">MILLER</span> </h1>
+      <img src={Headshot} className="m-1 rounded-full border-4 border-black w-1/2 h-auto" alt="My headshot" />
+      <p className="m-1 font-display text-center text-5xl">Full-Stack Engineer</p>
+      <p className="font-display text-center text-xl"><span className="text-highlightBlue text-2xl">☛</span>  Seeking Employment <span className="text-highlightBlue text-2xl">☚</span></p>
+      <p className="mb-1 font-display text-center text-8xl">React</p>
+      <p className="m-1 font-display text-center text-5xl">JavaScript</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'display': ['ultra','ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+      'body': ['ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif']
+    },
     extend: {
       colors: {
         highlightBlue: '#6575E0',
