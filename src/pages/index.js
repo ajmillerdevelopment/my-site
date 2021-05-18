1db398b8bdae2a6229c98db7ad3454c0f6054f4c
Alex Miller <ajm9594@gmail.com>
2021-05-18 15:14:02 -0700

    horizontal line

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ const IndexPage = () => {
       <h1 className="mt-4 mb-1 font-display text-center text-shadow text-highlightBlue"> <span className="text-4xl">ALEXANDER</span> <br /> <span className="text-7xl">MILLER</span> </h1>
       <img src={Headshot} className="m-1 rounded-full border-4 border-black w-1/2 h-auto" alt="My headshot" />
       <p className="m-1 font-display text-center text-5xl">Full-Stack Engineer</p>
-      <p className="font-display text-center text-xl"><span className="text-highlightBlue text-2xl">☛</span>  Seeking Employment <span className="text-highlightBlue text-2xl">☚</span></p>
+      <p className="font-display text-center text-xl"><span className="text-highlightBlue text-shadow-s text-2xl">☛</span> Seeking Employment <span className="text-highlightBlue text-shadow-s text-2xl">☚</span></p>
+      <div className="horizontal-line m-1 mt-2"></div>
       <p className="mb-1 font-display text-center text-8xl">React</p>
       <p className="m-1 font-display text-center text-5xl">JavaScript</p>
     </div>
